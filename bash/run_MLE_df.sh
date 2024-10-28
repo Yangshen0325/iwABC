14f76a0514ea50e8daa961f28f49c645dcd03989
Yangshen0325 <sylviadyx77@163.com>
2024-10-28 15:19:42 +0100

    work on develop branch

--- bash/run_MLE_df.sh
+++ bash/run_MLE_df.sh
@@ -8,6 +8,7 @@
 #SBATCH --partition=regular
 
 module load R-bundle-CRAN/2023.12-foss-2023a
+Rscript -e "devtools::install_github('rsetienne/DAISIE', ref = 'develop')"
 Rscript -e "devtools::install_github('Yangshen0325/iwABC')"
 
 Rscript ~/iwABC/script/job_MLE_df.R
