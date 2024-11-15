1833deef76dfa591b6b7064a7964b613f36cfd01
Y. Shen <89144088+Yangshen0325@users.noreply.github.com>
2024-11-15 11:47:17 +0100

    a smaller memory is enough

--- bash/run_MLE_df.sh
+++ bash/run_MLE_df.sh
@@ -4,10 +4,11 @@
 #SBATCH --ntasks=1
 #SBATCH --job-name=MLE_df
 #SBATCH --output=logs/MLE_df-%j.log
-#SBATCH --mem=30GB
+#SBATCH --mem=10GB
 #SBATCH --partition=regular
 
 module load R-bundle-CRAN/2023.12-foss-2023a
+
 Rscript -e "devtools::install_github('rsetienne/DAISIE', ref = 'develop')"
 Rscript -e "devtools::install_github('Yangshen0325/iwABC')"
 
