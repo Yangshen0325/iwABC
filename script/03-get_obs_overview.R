aa3c120dcb97d15a7c701eb4016e3985a47d6107
Y. Shen <89144088+Yangshen0325@users.noreply.github.com>
2025-01-22 13:30:04 +0100

    save the endeminism

--- script/03-get_obs_overview.R
+++ script/03-get_obs_overview.R
@@ -1,13 +1,14 @@
 
+rm(list=ls())
+# Read the observations
+iw_observations <- readRDS("script/iw_observations.rds")
 
-
-
-# Read initial parameters used in simulation for observed data
-parameter_space <- read.csv("script/parameter_space.csv")
 
 # Total number of species and clades
 # Initialize a data frame to store the summary
 summary_obs <- data.frame(total_sp = numeric(length(iw_observations)),
+                          end_sp = numeric(length(iw_observations)),
+                          nonend_sp = numeric(length(iw_observations)),
                           no_clades = numeric(length(iw_observations)))
 
 for (i in seq_along(iw_observations)) {
@@ -15,9 +16,25 @@ for (i in seq_along(iw_observations)) {
   stt_all <- iw_observations[[i]][[1]][["stt_all"]]
 
   summary_obs$total_sp[i] <- sum(stt_all[nrow(stt_all), c("nI", "nA", "nC")])
+  summary_obs$end_sp[i] <- sum(stt_all[nrow(stt_all), c("nA", "nC")])
+  summary_obs$nonend_sp[i] <- sum(stt_all[nrow(stt_all), c("nI")])
   summary_obs$no_clades[i] <- length(iw_observations[[i]]) - 1
 
 }
+saveRDS(summary_obs, file = "script/summary_obs.rds")
 
+
+# Viasulise ---------------------------------------------------------------
+
+library(tidyverse)
+
+summary_obs |>
+  pivot_longer(
+    cols = everything(),
+    names_to = "variable",
+    values_to = "value") |>
+  ggplot(aes(y = value)) +
+  geom_boxplot() +
+  facet_wrap(~variable, scales = "free")
 
 
