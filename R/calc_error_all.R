f58ea1731657271d21d58479b3a7f83fbbdaa3c0
Y. Shen <89144088+Yangshen0325@users.noreply.github.com>
2025-02-11 23:51:11 +0100

    add other summmary statistics

--- R/calc_error_all.R
+++ R/calc_error_all.R
@@ -1,6 +1,13 @@
 
 
 #' Calculate summary statistic distances between two simulated trees
+#' Diversity difference: `num_nonend`, `num_sington`, `num_multi`, `num_total`
+#' Phylogenetic difference: `clade_size`, `colon_time`
+#' NLTT difference:`total_nltt`, `nonend_nltt`, `singleton_nltt`, `multi_nltt`
+#' Number of colonization events difference: `num_colon`
+#' The largest clade size difference: `largest_cs_diff`
+#' The first clade size difference: `first_cs_diff`
+#' The proportion of the largest clades difference: `prop_largest_clade_diff`
 
 calc_error_all <- function(sim_1,
                            sim_2,
@@ -14,7 +21,6 @@ calc_error_all <- function(sim_1,
   lastRowId_sim_2 <- nrow(sim_2[[1]][["stt_all"]])
 
 
-  # difference of nonendemic species richness
   num_nonend_sim_1 <- as.numeric(sim_1[[1]][["stt_all"]][lastRowId_sim_1, "nI"])
   num_nonend_sim_2 <- as.numeric(sim_2[[1]][["stt_all"]][lastRowId_sim_2, "nI"])
   num_nonend <-
@@ -35,19 +41,6 @@ calc_error_all <- function(sim_1,
   num_total_sim_1 <- num_sington_sim_1 + num_multi_sim_1 + num_nonend_sim_1
   num_total_sim_2 <- num_sington_sim_2 + num_multi_sim_2 + num_nonend_sim_2
   num_total <- abs(num_total_sim_1 - num_total_sim_2)
-
-  num_end_sim_1 <- num_sington_sim_1 + num_multi_sim_1
-  num_end_sim_2 <- num_sington_sim_2 + num_multi_sim_2
-  num_end <- abs(num_end_sim_1 - num_end_sim_2)
-
-
-
-
-
-
-
-
-
 
   # Phylogenetic difference ---------------------------------------------------------
 
@@ -87,7 +80,7 @@ calc_error_all <- function(sim_1,
     normalize = FALSE
   )
 
-  ## nonendemic_nltt and singleton-endemic-nltt
+  ## nonendemic_nltt and singleton-endemic-nltt and multi-endemic-nltt
   end_ltt_1 <- end_ltt(sim_1, brt1)
   end_ltt_2 <- end_ltt(sim_2, brt2)
 
@@ -126,6 +119,29 @@ calc_error_all <- function(sim_1,
     )
   }
 
+  multi_ltt_1 <- end_ltt_1$multi_ltt
+  multi_ltt_2 <- end_ltt_2$multi_ltt
+  # total number of cladogenetic species nltt error
+  if (multi_ltt_1[1, 1] == 0 && multi_ltt_2[1, 1] == 0) {
+    multi_nltt <- 0
+  } else {
+    multi_nltt <- nLTT::nltt_diff_exact_extinct(
+      event_times = multi_ltt_1$multi_brt,
+      species_number = multi_ltt_1$n_multi,
+      event_times2 = multi_ltt_2$multi_brt,
+      species_number2 = multi_ltt_2$n_multi,
+      distance_method = distance_method,
+      time_unit = "ago",
+      normalize = FALSE
+    )
+  }
+
+
+  # No.Colonization event difference ------------------------------------------------------
+
+  num_colon_1 <- M - sim_1[[1]][["not_present"]]
+  num_colon_2 <- M - sim_2[[1]][["not_present"]]
+  num_colon <- abs(num_colon_1 - num_colon_2)
 
 
 
@@ -138,6 +154,24 @@ calc_error_all <- function(sim_1,
 
 
 
+
+  # the largest clade size difference ---------------------------------------
+  largest_clade_size_1 <- largest_clade_size(sim_1)
+  largest_clade_size_2 <- largest_clade_size(sim_2)
+  largest_cs_diff <- abs(largest_clade_size_1 - largest_clade_size_2)
+
+
+  # proportion of the largest clades ----------------------------------------
+  prop_largest_clade_1 <- largest_clade_size_1 / num_total_sim_1
+  prop_largest_clade_2 <- largest_clade_size_2 / num_total_sim_2
+  prop_largest_clade_diff <- abs(prop_largest_clade_1 - prop_largest_clade_2)
+
+
+
+  # the first clade size ----------------------------------------------------
+  first_clade_size_1 <- first_clade_size(sim_1)
+  first_clade_size_2 <- first_clade_size(sim_2)
+  first_cs_diff <- abs(first_clade_size_1 - first_clade_size_2)
 
 
   # All errors --------------------------------------------------------------
@@ -145,12 +179,18 @@ calc_error_all <- function(sim_1,
     c(
       total_nltt,
       singleton_nltt,
+      multi_nltt,
       nonend_nltt,
       colon_time,
       clade_size,
       num_total,
-      num_end,
-      num_nonend
+      num_sington,
+      num_multi,
+      num_nonend,
+      num_colon,
+      largest_cs_diff,
+      first_cs_diff,
+      prop_largest_clade_diff
     )
   )
 }
