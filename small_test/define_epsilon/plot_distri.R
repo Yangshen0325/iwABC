f30e3a1d24d7ba4d998b702f24f4c090f3e9ab38
Y. Shen <89144088+Yangshen0325@users.noreply.github.com>
2025-02-16 16:23:36 +0100

    new initial epsilon analysis

--- small_test/define_epsilon/plot_distri.R
+++ small_test/define_epsilon/plot_distri.R
@@ -1,33 +1,37 @@
 # Visualize ---------------------------------------------------------------
-
+rm(list=ls())
 # read data
-t_ss_distri <- readRDS("~/Downloads/phd_yang/pkgs/iwABC/small_test/define_epsilon/t_ss_distri.rds")
+t_ss_distri <- readRDS("small_test/define_epsilon/t_ss_distri.rds")
 # format data
-ss_diff_list <- lapply(t_ss_distri, function(x) x$ss_diff_list[[1]])
+groups <- split(t_ss_distri, rep(1:32, each = 10))
 
-# set column names
-column_names <- c(
-  "diff_total_nltt", "diff_singleton_nltt", "diff_multi_nltt", "diff_nonend_nltt",
-  "diff_colon_time", "diff_clade_size",
-  "diff_total", "diff_sington", "diff_multi", "diff_noend",
-  "diff_colon",
-  "diff_largest_cs", "diff_first_cs",
-  "diff_prop_largest_clade"
-)
-ss_diff_table <- lapply(ss_diff_list, function(mat) {
-  colnames(mat) <- column_names
-  return(mat)
+col_names <- c("diff_nonend", "diff_singleton", "diff_multi",
+               "diff_nonend_nltt", "diff_singleton_nltt", "diff_multi_nltt",
+               "diff_sd_cs", "diff_largest_cs", "diff_first_cs", "diff_prop_largest_clade",
+               "diff_sd_colontime", "diff_colon")
+
+groups <- lapply(groups, function(group) {
+  lapply(group, function(mat) {
+    # If mat is a matrix, convert it to a data frame for easier handling.
+    df <- as.data.frame(mat)
+    colnames(df) <- col_names
+    return(df)
+  })
+})
+# For each group, add a "Group" column and then combine all replicates.
+combined_list <- lapply(seq_along(groups), function(g) {
+  # groups[[g]] is a list of 10 data frames for group g.
+  lapply(groups[[g]], function(df) {
+    df$Group <- g  # Add group identifier
+    return(df)
+  })
 })
 
-# combine data across scenarios
-combined_data <- do.call(rbind, lapply(seq_along(ss_diff_table), function(i) {
-  data <- as.data.frame(ss_diff_table[[i]])
-  data$Scenario <- i  # Add a column for the scenario number
-  return(data)
-}))
+# Unlist the nested lists (flatten to one list of data frames)
+flat_list <- unlist(combined_list, recursive = FALSE)
 
-
-
+# Combine all data frames into one data frame.
+combined_data <- do.call(rbind, flat_list)
 
 
 # Plotting
@@ -36,21 +40,21 @@ library(tidyverse)
 # longer data
 long_data <- combined_data  |>
   pivot_longer(
-    cols = -Scenario,
+    cols = -Group,
     names_to = "Statistic",
     values_to = "Difference"
   )
 
 # Define plot functions
 plot_box <- function(data, title) {
-  ggplot(data, aes(x = as.factor(Scenario), y = Difference, fill = as.factor(Scenario))) +
+  ggplot(data, aes(x = as.factor(Group), y = Difference, fill = as.factor(Group))) +
     geom_boxplot(outlier.alpha = 0.3) +
     facet_wrap(~Statistic, scales = "free", ncol = 1) +
     labs(
       title = title,
-      x = "Scenario",
+      x = "Group",
       y = "Difference",
-      fill = "Scenario"
+      fill = "Group"
     ) +
     theme_minimal() +
     theme(legend.position = "none")
@@ -58,25 +62,24 @@ plot_box <- function(data, title) {
 
 
 # group summary statistics for plotting
-groups <- list(
-  group1 = c("diff_total_nltt", "diff_singleton_nltt", "diff_multi_nltt", "diff_nonend_nltt"),
-  group2 = c("diff_colon_time", "diff_clade_size", "diff_colon"),
-  group3 = c("diff_total", "diff_songton", "diff_multi", "diff_noend"),
-  group4 = c("diff_largest_cs", "diff_first_cs", "diff_prop_largest_clade")
-
+types <- list(
+  type1 = c("diff_nonend", "diff_singleton", "diff_multi"),
+  type2 = c("diff_nonend_nltt", "diff_singleton_nltt", "diff_multi_nltt"),
+  type3 = c("diff_sd_cs", "diff_largest_cs", "diff_first_cs", "diff_prop_largest_clade"),
+  type4 = c("diff_sd_colontime", "diff_colon")
 )
 
 # box plot
-for (group_name in names(groups)) {
+for (type_name in names(types)) {
 
-  group <- groups[[group_name]]
+  the_type <- types[[type_name]]
 
   # Filter data for the group
-  group_data <- long_data  |>
-    filter(Statistic %in% group)
+  type_data <- long_data  |>
+    filter(Statistic %in% type)
 
   # Create box plot
-  box_plot <- plot_box(group_data, paste("Box Plot for", group_name))
+  box_plot <- plot_box(type_data, paste("Box Plot for", type_name))
   print(box_plot)
 
 }
@@ -93,51 +96,49 @@ summary_stats <- long_data  |>
 
 print(summary_stats)
 
-# A tibble: 14 × 4
-# Statistic               median Percentile_95 max_val
-# <chr>                    <dbl>         <dbl>   <dbl>
-#   1 diff_clade_size          1.13          3.63   24.6
-# 2 diff_colon               8            24      47
-# 3 diff_colon_time          0.299         1.18    2.43
-# 4 diff_first_cs            2            10      46
-# 5 diff_largest_cs          4            13      45
-# 6 diff_multi              14            56      82
-# 7 diff_multi_nltt         34.0         133.    265.
-# 8 diff_noend               3             9      45
-# 9 diff_nonend_nltt         4.53         22.4   118.
-# 10 diff_prop_largest_clade  0.124         0.538   0.898
-# 11 diff_singleton_nltt     12.7          47.1   152.
-# 12 diff_sington             4            16      45
-# 13 diff_total              17            59      91
-# 14 diff_total_nltt         42.0         145.    293.
+# A tibble: 12 × 4
+# Statistic                median Percentile_95 max_val
+# <chr>                     <dbl>         <dbl>   <dbl>
+#   1 diff_colon              888           901     907
+# 2 diff_first_cs             2            14      57
+# 3 diff_largest_cs           3            15      57
+# 4 diff_multi                9            43      87
+# 5 diff_multi_nltt          20.7         108.    280.
+# 6 diff_nonend               2            11      49
+# 7 diff_nonend_nltt          2.18         19.2   147.
+# 8 diff_prop_largest_clade   0.467         0.909   0.982
+# 9 diff_sd_colontime         0.869         1.75    3.48
+# 10 diff_sd_cs                1.06          5.51   36.0
+# 11 diff_singleton            5            18      54
+# 12 diff_singleton_nltt      15.1          54.4   187.
 
 # If you want to plot density plot (too many lines, not recommended) --------
 
 
 plot_density <- function(data, title) {
-  ggplot(data, aes(x = Difference, fill = as.factor(Scenario), color = as.factor(Scenario))) +
+  ggplot(data, aes(x = Difference, fill = as.factor(Group), color = as.factor(Group))) +
     geom_density(alpha = 0.3) +
     facet_wrap(~Statistic, scales = "free", ncol = 1) +
     labs(
       title = title,
       x = "Difference",
       y = "Density",
-      fill = "Scenario",
-      color = "Scenario"
+      fill = "Group",
+      color = "Group"
     ) +
     theme_minimal()
 }
 
-# box plot
-for (group_name in names(groups)) {
+# density plot
+for (type_name in names(types)) {
 
-  group <- groups[[group_name]]
+  the_type <- types[[type_name]]
 
   # filter data
-  group_data <- long_data  |>
-    filter(Statistic %in% group)
+  type_data <- long_data  |>
+    filter(Statistic %in% type)
 
-  density_plot <- plot_density(group_data, paste("Density Plot for", group_name))
+  density_plot <- plot_density(type_data, paste("Density Plot for", type_name))
   print(density_plot)
 
 }
