d9d5cb04b1b16cebb556ce96480002c132426fc7
Y. Shen <89144088+Yangshen0325@users.noreply.github.com>
2025-03-13 20:54:39 +0100

    changes from Thijs

--- script/job_run_ABC.R
+++ script/job_run_ABC.R
@@ -1,16 +1,18 @@
 args <- commandArgs(TRUE)
 
+args <- c(1, 1, 2, 3, 4, 5)
+
 param_set <- as.numeric(args[1])
 idparsopt_lac <- as.numeric(args[2])
 idparsopt_mu <- as.numeric(args[3])
 idparsopt_K <- as.numeric(args[4])
 idparsopt_gam <- as.numeric(args[5])
 idparsopt_laa <- as.numeric(args[6])
-ss_set <- as.numeric(args[7])
+ss_set <- 0 # as.numeric(args[7])
 
 idparsopt_all <- c(idparsopt_lac, idparsopt_mu, idparsopt_K, idparsopt_gam, idparsopt_laa)
 
-idparsopt <- which(idparsopt_all == 1)
+idparsopt <- idparsopt_all # which(idparsopt_all == 1)
 
 saveOrNot <- TRUE
 
@@ -20,9 +22,44 @@ metadata <- paste0("This is parameter set ", param_set)
 
 library(iwABC)
 
+t0 <- Sys.time()
+
 iwABC::run_ABC(
   param_set = as.numeric(args[1]),
   idparsopt = as.numeric(idparsopt),
   saveOrNot = saveOrNot,
-  ss_set = 0
+  ss_set = 0,
+  number_of_particles = 100,
+  num_iterations = 7,
+  num_threads = 8
 )
+
+t1 <- Sys.time()
+
+difftime(t1, t0)
+
+want_to_plot <- FALSE
+
+if (want_to_plot) {
+
+
+  require(tidyverse)
+
+  res <- readRDS("/Users/thijsjanzen/Documents/GitHub/iwABC/results/param_set_1_ss_0.rds")
+
+  to_plot <- c()
+  for (i in 1:length(res$ABC)) {
+    to_add <- cbind(i, res$ABC[[i]])
+    to_plot <- rbind(to_plot, to_add)
+  }
+
+  colnames(to_plot) <- c("iteration", "lac", "mu", "K", "gam", "laa")
+  to_plot <- as_tibble(to_plot)
+  to_plot %>%
+    gather(key = parameter, value = "value", -iteration) %>%
+    ggplot(aes(x = iteration, y = value, group = iteration)) +
+    geom_boxplot() +
+    scale_y_log10() +
+    facet_wrap(~parameter, scales = "free")
+}
+
