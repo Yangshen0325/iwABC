0777db4d39786023d67bb03d3519c275ca9c46a8
Y. Shen <89144088+Yangshen0325@users.noreply.github.com>
2025-03-14 14:40:58 +0100

    comment out the test code line

--- script/job_run_ABC.R
+++ script/job_run_ABC.R
@@ -1,6 +1,7 @@
 args <- commandArgs(TRUE) # use command-line arguments passed when executing the script
 
-args <- c(1, 1, 2, 3, 4, 5)
+# Uncomment for local testing:
+# args <- c(1, 1, 2, 3, 4, 5)
 
 param_set <- as.numeric(args[1])
 idparsopt_lac <- as.numeric(args[2])
@@ -8,7 +9,7 @@ idparsopt_mu <- as.numeric(args[3])
 idparsopt_K <- as.numeric(args[4])
 idparsopt_gam <- as.numeric(args[5])
 idparsopt_laa <- as.numeric(args[6])
-ss_set <- 0 # as.numeric(args[7])
+ss_set <- as.numeric(args[7])
 
 idparsopt_all <- c(idparsopt_lac, idparsopt_mu, idparsopt_K, idparsopt_gam, idparsopt_laa)
 
