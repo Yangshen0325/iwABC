a63df4637b3d248f12bdd5442886e4c37c201ba3
Y. Shen <89144088+Yangshen0325@users.noreply.github.com>
2025-04-20 19:17:22 +0200

    function of calculate the rank of the largest calde

--- R/calc_clade_size_error.R
+++ R/calc_clade_size_error.R
@@ -58,3 +58,29 @@ first_clade_size <- function(sim){
   return(first_clade_size)
 }
 
+
+
+# Get the rank of the largest clade based on colonization time
+get_rank_largest_clade <- function(sim) {
+
+  if (length(sim) == 1){
+    rank_largest <- 0
+  } else {
+
+    clade_sublists <- sim[2:length(sim)]  # exclude sim[[1]]
+    # Get clade sizes and colonization times
+    clade_sizes <- sapply(clade_sublists, function(clade) {
+      length(clade$branching_times) - 1})
+    colonization_times <- sapply(clade_sublists, function(clade) {
+      clade$branching_times[2]})
+
+     # Find index of largest clade
+    index_largest_clade <- which.max(clade_sizes)
+    # Rank clades by colonization time (bigger = older = ealier)
+    colonization_rank <- order(colonization_times, decreasing = TRUE)
+    # Find the rank of the largest clade
+    rank_largest <- match(index_largest_clade, colonization_rank)
+  }
+}
+
+
