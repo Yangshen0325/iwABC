6a84e90ad07a008579922f42b56aee8fe3586076
Y. Shen <89144088+Yangshen0325@users.noreply.github.com>
2025-04-22 16:31:40 +0200

    for computing clade evenness

---
+++ R/calc_clade_evenness.R
@@ -0,0 +1,26 @@
+
+#library(vegan)  # for diversity metrics
+# calculate the clade evenness
+
+calc_clade_evenness <- function(sim) {
+  if (length(sim) == 1) {
+    J <- NA # no clades
+  } else {
+    clade_sublists <- sim[2:length(sim)] # exclude sim[[1]]
+    clade_sizes <- sapply(clade_sublists, function(clade) {
+      length(clade$branching_times) - 1
+    })
+
+    S <- length(clade_sizes) # number of clades
+
+    if (S <= 1) {
+      return(NA)
+    }
+
+    # Shannon evenness
+    H <- diversity(clade_sizes, index = "shannon")
+    J <- H / log(S)
+
+    return(J) # value between 0 (uneven) and 1 (perfect evenness)
+  }
+}
