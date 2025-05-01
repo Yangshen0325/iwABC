e085d9b832e4834f8dbdb757c1f92207bb5936f5
Y. Shen <89144088+Yangshen0325@users.noreply.github.com>
2025-05-01 21:52:16 +0200

    add NLTT metric

--- R/calc_all_stats.R
+++ R/calc_all_stats.R
@@ -6,47 +6,91 @@
 
 calc_all_stats <- function(sim) {
 
+
+# # ABC SR ----------------------------------------------------------------
+
+
   lastRowId_sim <- nrow(sim[[1]][["stt_all"]])
   num_nonend_sim <- as.numeric(sim[[1]][["stt_all"]][lastRowId_sim, "nI"])
   num_sington_sim <- as.numeric(sim[[1]][["stt_all"]][lastRowId_sim, "nA"])
   num_multi_sim <- as.numeric(sim[[1]][["stt_all"]][lastRowId_sim, "nC"])
 
-  #brt <- lapply(sim[-1], "[[", "branching_times")
-  #ltt <- full_ltt(sim, brt)
-  #end_ltt <- end_ltt(sim, brt)
 
-  #nonend_ltt <- end_ltt$nonend_ltt
-  #singleton_ltt <- end_ltt$singleton_ltt
-  #multi_ltt <- end_ltt$multi_ltt
+# ABC NLTT ----------------------------------------------------------------
 
-  #sim_cs_sd <- clade_size_sd(sim)
+  brt_sim <- lapply(sim[-1], "[[", "branching_times")
+  end_ltt_sim <- end_ltt(sim, brt_sim)
+  nonend_ltt <- end_ltt_sim$nonend_ltt
+  singleton_ltt <- end_ltt_sim$singleton_ltt
+  multi_ltt <- end_ltt_sim$multi_ltt
+  #NLTT-nonend
+  nonend_nltt <- nLTT::nltt_diff_exact_extinct(
+    event_times = nonend_ltt$nonend_brt,
+    species_number = nonend_ltt$n_nonend,
+    event_times2 = 0,
+    species_number2 = 0,
+    distance_method = "abs",
+    time_unit = "ago",
+    normalize = FALSE
+  )
+  if (length(nonend_nltt) == 0) 0 else nonend_nltt
+  #NLTT-singleton
+  singleton_nltt <- nLTT::nltt_diff_exact_extinct(
+    event_times = singleton_ltt$singleton_brt,
+    species_number = singleton_ltt$n_singleton,
+    event_times2 = 0,
+    species_number2 = 0,
+    distance_method = "abs",
+    time_unit = "ago",
+    normalize = FALSE
+  )
+  if (length(singleton_nltt) == 0) 0 else singleton_nltt
+  #NLTT-multi
+  multi_nltt <- nLTT::nltt_diff_exact_extinct(
+    event_times = multi_ltt$multi_brt,
+    species_number = multi_ltt$n_multi,
+    event_times2 = 0,
+    species_number2 = 0,
+    distance_method = "abs",
+    time_unit = "ago",
+    normalize = FALSE
+  )
+  if (length(multi_nltt) == 0) 0 else multi_nltt
 
-  largest_clade_size <- largest_clade_size(sim)
-
+  # ABC CD ----------------------------------------------------------------
+  # first clade
   first_clade_size <- first_clade_size(sim)
-
+  # proportion of the largest clade
+  largest_clade_size <- largest_clade_size(sim)
   num_total_sim <- num_sington_sim + num_multi_sim + num_nonend_sim
   prop_largest_clade <- largest_clade_size / num_total_sim
-
+  # rank of the largest clade
   rank_largest_clade <- get_rank_largest_clade(sim)
-
+  # clade evenness
+  clade_evenness <- calc_clade_evenness(sim)
+  # colonisation time standard deviation
   sim_ct_sd <- colon_time_sd(sim)
-
+  # number of colonisation
   num_colon <- 1000 - sim[[1]][["not_present"]]
 
-  clade_evenness <- calc_clade_evenness(sim)
+
+# return  -----------------------------------------------------------------
+
 
   return(tibble::tibble(
     num_nonend_sim = num_nonend_sim,
     num_sington_sim = num_sington_sim,
     num_multi_sim = num_multi_sim,
+    nonend_nltt = nonend_nltt,
+    singleton_nltt = singleton_nltt,
+    multi_nltt = multi_nltt,
     #sim_cs_sd = sim_cs_sd,
     #largest_clade_size = largest_clade_size,
     first_clade_size = first_clade_size,
     prop_largest_clade = prop_largest_clade,
     rank_largest_clade = rank_largest_clade,
+    clade_evenness = clade_evenness,
     sim_ct_sd = sim_ct_sd,
-    num_colon = num_colon,
-    clade_evenness = clade_evenness
+    num_colon = num_colon
   ))
 }
