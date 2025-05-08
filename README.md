527ef847e6e0ce243eda98bbbb33b90d9d392828
Hanno Hildenbrandt <h.hildenbrandt@rug.nl>
2025-05-08 14:27:17 +0200

    nop

--- README.md
+++ README.md
@@ -9,6 +9,13 @@ git checkout hanno
 
 ## Before we submit any jobs
 
+Do you have `.Rprofile` setup? If not, create one with:
+
+```
+echo ".libPaths( c('~/.R/libs', .libPaths()))" >> ~/.Rprofile
+```
+
+
 *Source* the [preparation script](./bash/prep.sh).<br>
 We need to do this only once.<br>
 R packages will be installed in `~/.R/libs` - available cluster-wide.
--- bash/prep.sh
+++ bash/prep.sh
@@ -5,10 +5,7 @@
 
 if [ "$1" != "--local" ]; then
     module load R-bundle-CRAN/2023.12-foss-2023a
-else 
-    # default on Habrok, mkae it consistent on local machine
-    Rscript -e ".libPaths( c('~/.R/libs', .libPaths()))"
 fi
-Rscript -e "install.packages(c('optparse', 'devtools'), repos='https://cloud.r-project.org')"
-Rscript -e "devtools::install_github('rsetienne/DAISIE', repos = 'https://github.com', ref = 'develop')"
-Rscript -e "devtools::install()"
+Rscript -e ".libPaths('~/.R/libs'); install.packages(c('optparse', 'devtools'), lib='~/.R/libs', repos='https://cloud.r-project.org')"
+Rscript -e ".libPaths('~/.R/libs'); devtools::install_github('rsetienne/DAISIE', repos = 'https://github.com', ref = 'develop')"
+Rscript -e ".libPaths('~/.R/libs'); devtools::install()"
