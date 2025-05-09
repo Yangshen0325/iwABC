15d052a0274e6947e900cbc3eef67a0605bcfda0
Hanno Hildenbrandt <h.hildenbrandt@rug.nl>
2025-05-09 15:07:28 +0200

    nop

--- bash/run_MLE_df.sh
+++ bash/run_MLE_df.sh
@@ -2,13 +2,14 @@
 #SBATCH --time=6-10:55:00
 #SBATCH --nodes=1
 #SBATCH --ntasks=1
-#SBATCH --cpus-per-task=8              # request 8 cores per job
+#SBATCH --cpus-per-task=16              # request 16 cores per job
 #SBATCH --job-name=MLE_df_nomc
-#SBATCH --mem=10GB
+#SBATCH --mem=8GB
 #SBATCH --partition=regular
 #SBATCH --array=1-48                    # one task per parameter set
 #SBATCH --output=logs/MLE_df_nomc-%A_%a.log
 
-Rscript ~/iwABC/script/mle_job.R \
+Rscript ~/iwABC/script/job_MLE_df.R \
    --index $SLURM_ARRAY_TASK_ID \
-   --njobs $SLURM_ARRAY_TASK_COUNT
+   --njobs $SLURM_ARRAY_TASK_COUNT \
+   > /dev/null 2>&1
