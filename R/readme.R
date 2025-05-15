88545555bf09768d654cc8629949a968c28e95dc
Y. Shen <89144088+Yangshen0325@users.noreply.github.com>
2025-05-15 14:25:59 +0200

    a note

---
+++ R/readme.R
@@ -0,0 +1,8 @@
+##### Read Me
+
+# Only run "script/02-get_observations.R", there you will get 48 sublists for each combinations
+# of parameters in "data/parameter_space.csv", and each sublist has a length of 1000 reps, each rep
+# represents an island phylogeny.
+
+# Run MLE in order to get 100 estimations from 1000 reps for each combination of parameters, stop
+# when 100 results has in hands.
