e02a4bd66e28ff2ba953c285608677aff7da37f9
Y. Shen <89144088+Yangshen0325@users.noreply.github.com>
2025-05-23 20:58:24 +0200

    improve

--- bash/start_ABC.sh
+++ bash/start_ABC.sh
@@ -2,25 +2,27 @@
 #SBATCH --time=3-10:55:00
 #SBATCH --nodes=1
 #SBATCH --ntasks=1
-#SBATCH --cpus-per-task=1      # Allocate 8 CPU cores for this task
+#SBATCH --cpus-per-task=1      #
 #SBATCH --job-name=job_run_ABC
 #SBATCH --output=logs/ABC-%j.log
 #SBATCH --mem=10GB
 #SBATCH --partition=regular
 
-# Arguments to follow the Rscript are as follows:
-param_set=${1}
-idparsopt_lac=${2}
-idparsopt_mu=${3}
-idparsopt_K=${4}
-idparsopt_gam=${5}
-idparsopt_laa=${6}
-ss_set=${7}
+# Check number of arguments
+if [ $# -ne 7 ]; then
+  echo "Usage: sbatch start_ABC.sh <param_set> <lac> <mu> <K> <gam> <laa> <ss_set>"
+  exit 1
+fi
 
+param_set=$1
+idparsopt_lac=$2
+idparsopt_mu=$3
+idparsopt_K=$4
+idparsopt_gam=$5
+idparsopt_laa=$6
+ss_set=$7
 
 module load R-bundle-CRAN/2023.12-foss-2023a
-Rscript -e "devtools::install_github('rsetienne/DAISIE', ref = 'develop')"
-Rscript -e "devtools::install_github('Yangshen0325/iwABC')"
 
 Rscript ~/iwABC/script/job_run_ABC.R ${param_set} \
                                      ${idparsopt_lac} \
