1595fb2eaf3c83a3a31c9b51e708971b6d6bf836
Y. Shen <89144088+Yangshen0325@users.noreply.github.com>
2025-07-22 19:19:30 +0800

    prior bounds should cover true values!

--- R/prior_functions.R
+++ R/prior_functions.R
@@ -5,31 +5,31 @@
 prior_dens <- function(pars, idparsopt) {
 
   if(1 %in% idparsopt){
-    dens_lac <- stats::dunif(pars[1], 0, 2)
+    dens_lac <- stats::dunif(pars[1], 0, 1)
   } else {
     dens_lac <- 1
   }
 
   if(2 %in% idparsopt){
-    dens_mu <- stats::dunif(pars[2], 0, 2)
+    dens_mu <- stats::dunif(pars[2], 0, 0.5)
   } else {
     dens_mu <- 1
   }
 
   if(3 %in% idparsopt){
-    dens_k <- stats::dunif(pars[3], 0, 500)
+    dens_k <- stats::dunif(pars[3], 0, 2000)
   } else {
     dens_k <- 1
   }
 
   if(4 %in% idparsopt){
-    dens_gam <- stats::dunif(pars[4], 0, 0.02)
+    dens_gam <- stats::dunif(pars[4], 0, 0.05)
   } else {
     dens_gam <- 1
   }
 
   if(5 %in% idparsopt){
-    dens_laa <- stats::dunif(pars[5], 0, 2)
+    dens_laa <- stats::dunif(pars[5], 0, 1.5)
   } else {
     dens_laa <- 1
   }
@@ -57,13 +57,13 @@ prior_gen <- function(pars,idparsopt){
   }
 
   if(3 %in% idparsopt){
-    k <- stats::runif(1, 0, 100)
+    k <- stats::runif(1, 0, 2000)
   } else {
     k <- pars[3]
   }
 
   if(4 %in% idparsopt){
-    gam <- stats::runif(1, 0, 0.02)
+    gam <- stats::runif(1, 0, 0.05)
   } else {
     gam <- pars[4]
   }
