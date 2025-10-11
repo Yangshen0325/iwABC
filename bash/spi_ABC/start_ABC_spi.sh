36119be35424338a3b052b77b6239ceabd550e23
Y. Shen <89144088+Yangshen0325@users.noreply.github.com>
2025-10-11 11:20:18 +0800

    submit all param_set, tesing

--- bash/spi_ABC/start_ABC_spi.sh
+++ bash/spi_ABC/start_ABC_spi.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
-#SBATCH --time=4:00:00
+#SBATCH --time=1:00:00
 #SBATCH --nodes=1
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=1      #
 #SBATCH --job-name=run_ABC_spi_f10
 #SBATCH --output=logsFirstTen/ABC_spi-%j.log
-#SBATCH --mem=10GB
+#SBATCH --mem=5GB
 #SBATCH --partition=regular
 
 # Ensure log directory exists (avoids 'No such file or directory' on some systems)
