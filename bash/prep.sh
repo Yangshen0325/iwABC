f2077597779e3a928751067f61dbe3d8a6b073c9
Y. Shen <89144088+Yangshen0325@users.noreply.github.com>
2025-11-21 20:57:53 +0800

    using the main branch

--- bash/prep.sh
+++ bash/prep.sh
@@ -7,6 +7,6 @@ if [ "$1" != "--local" ]; then
 module load R-bundle-CRAN/2023.12-foss-2023a
 fi
 # Rscript -e "install.packages(c('optparse', 'devtools', 'DAISIE'), repos='https://cloud.r-project.org')"
-Rscript -e "devtools::install_github('rsetienne/DAISIE', repos = 'https://github.com', ref = 'develop')"
+Rscript -e "devtools::install_github('rsetienne/DAISIE')"
 Rscript -e "devtools::install_github('thijsjanzen/DAISIEiwsim')"
 Rscript -e "devtools::install_github('Yangshen0325/iwABC')"
