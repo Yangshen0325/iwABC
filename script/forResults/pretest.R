4326c0b985e872cd824d937391d3b1d82337789f
Y. Shen <89144088+Yangshen0325@users.noreply.github.com>
2026-01-16 11:11:47 +0800

    pretest data from Thijs

--- script/forResults/pretest.R
+++ script/forResults/pretest.R
@@ -1,18 +1,21 @@
 
+rm(list = ls())
 library(tidyverse)
 ##### Preview some results
 
 res <- readRDS("/Users/yangshen/Downloads/phd_yang/ABC_IW/param_set_1_ss_0.rds")
 
 # true parameters
-parameter_space <- read.csv("data/parameter_space.csv")
+parameter_space <- read.csv("data/parameter_space_rep100_large_k.csv")
 
 unique_parameter_space <- parameter_space |>
   select(lac, mu, K, gam, laa) |>
   distinct()
 
 # For this example, the true parameters are the first row of the parameter space
-true_parameters <- unique_parameter_space[1, ]
+true_parameters <- unique_parameter_space[2, ]
+
+print(true_parameters)
 
 to_plot <- c()
 for (i in 1:length(res$ABC)) {
