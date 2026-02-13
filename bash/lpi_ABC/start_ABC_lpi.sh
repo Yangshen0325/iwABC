ec6e994f4dd00cdec46fc76846b18ce6893d1f1a
Y. Shen <89144088+Yangshen0325@users.noreply.github.com>
2026-02-13 15:45:26 +0800

    7days, 30g, ys node

--- bash/lpi_ABC/start_ABC_lpi.sh
+++ bash/lpi_ABC/start_ABC_lpi.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
-#SBATCH --time=4-00:00:00
+#SBATCH --time=7-00:00:00
 #SBATCH --nodes=1
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=1      #
 #SBATCH --job-name=ys_run_ABC_lpi
 #SBATCH --output=logsFirstTen/ys_run_ABC_lpi-%j.log
-#SBATCH --mem=40GB
+#SBATCH --mem=30GB
 #SBATCH --partition=regular
 
 # Ensure log directory exists (avoids 'No such file or directory' on some systems)
